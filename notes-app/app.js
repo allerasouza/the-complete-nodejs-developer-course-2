260b260e8fc90d44c719a8be033bcfc8ef3948d9
Aller Augusto de Souza <allerasouza@gmail.com>
2023-02-16 08:03:07 -0300

    feat: import module

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,14 +1,7 @@
-// const add = require('./utils.js')
-// const sum = add(4, -2)
-// console.log(sum)
-
-//
-// Challenge: Define and use a function in a new file
-//
-// 1. Create a new file called notes.js
-// 2. Create getNotes function that returns "Your notes..."
-// 3. Export getNotes function
-// 4. From  app.js, load in and call the function printing message to console
+const validator = require('validator');
 
 const getNotes = require('./notes.js');
 console.log(getNotes());
+
+console.log(validator.isEmail('andrew@example.com'));
+console.log(validator.isURL('https://mead.io'));
