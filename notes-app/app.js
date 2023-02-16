5e1bee3143f2f29607bdb009e54e42e389bc0f98
Aller Augusto de Souza <allerasouza@gmail.com>
2023-02-16 07:44:46 -0300

    feat: import local files

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,12 +1,14 @@
-const fs = require('fs');
-
-fs.writeFileSync('notes.txt', 'This file was created by Node.js!');
+// const add = require('./utils.js')
+// const sum = add(4, -2)
+// console.log(sum)
 
 //
-// Challenge: Append a message to notes.txt
+// Challenge: Define and use a function in a new file
 //
-// 1. Use appendFileSync to append to the file
-// 2. Run the script
-// 3. Check you work by opening the file and viewing the appended text
+// 1. Create a new file called notes.js
+// 2. Create getNotes function that returns "Your notes..."
+// 3. Export getNotes function
+// 4. From  app.js, load in and call the function printing message to console
 
-fs.appendFileSync('notes.txt', ' Appended message!');
+const getNotes = require('./notes.js');
+console.log(getNotes());
