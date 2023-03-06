6473c3e5877f341d6b635817a1a35e8e3c542924
Aller Augusto de Souza <allerasouza@gmail.com>
2023-03-06 16:30:58 -0300

    feat: read a note

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -38,6 +38,17 @@ const listNotes = () => {
   notes.forEach(note => console.log(note.title));
 }
 
+const readNote = (title) => {
+  const notes = loadNotes();
+  const note = notes.find((note) => note.title === title);
+  if(note) {
+    console.log(chalk.inverse(note.title));
+    console.log(note.body);
+  } else {
+    console.log(chalk.red.inverse("Note not found!"));
+  }
+}
+
 const saveNotes = (notes) => {
   const dataJSON = JSON.stringify(notes);
   fs.writeFileSync('notes.json', dataJSON);
@@ -57,5 +68,6 @@ module.exports = {
   getNotes: getNotes,
   addNote: addNote,
   removeNote: removeNote,
-  listNotes: listNotes
+  listNotes: listNotes,
+  readNote: readNote
 }
